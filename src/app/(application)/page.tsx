eb4272b0ca394e3820c796ec1a5a74913aa59e2e
Gustavo <132913165+gustavo94gm@users.noreply.github.com>
2024-03-21 12:23:12 -0300

    feat: landing page

---
+++ src/app/(application)/page.tsx
@@ -0,0 +1,22 @@
+import './style.css'
+
+export default async function Page() {
+    return (
+        <div>
+        <div className="header">
+            <div className="sidebar">
+        <div className="sidebar-item">DOC2X</div>
+        <div className="sidebar-right">
+        <div className="sidebar-item"><a href="">Item 1</a></div>
+        <div className="sidebar-item"><a href="">Item 2</a></div>
+        <div className="sidebar-item"><a href="">Item 3</a></div>
+      </div>
+    </div>
+        </div>
+        <div className="body">
+        <h1>Follow all the projects published here!</h1>
+        
+        </div>
+        </div>
+    )
+}
